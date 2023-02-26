dd9643785edcce9cb3240d4e77b91f7ce9530732
Crislane Alves <crislanealves@protonmail.com>
2023-02-26 13:34:59 +0000

    feat: add setup de instalação

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "Verificando compatibilidade do sistema operacional ..."
+
+sudo apt-get install apt-transport-https ca-certificates gnupg
+
+echo "Iniciando a instalação"
+
+echo "Passo 1: Executando comando de compatibilidade"
+
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+
+echo "Passo 2: Importando a chave pública do Google Cloud"
+
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+
+echo "Passo 3: Atualizando e instalando a CLI gcloud"
+
+sudo apt-get update && sudo apt-get install google-cloud-cli
+
+echo "Passo 4: Autenticação"
+
+gcloud auth login
+
+
+
+

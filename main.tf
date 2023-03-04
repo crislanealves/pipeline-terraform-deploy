e89f5a99a4419bd268b7def9a4e7e6e0a4e6158c
crislanealves <s.alvescrislane@gmail.com>
2023-03-04 09:15:00 -0300

    fix: organiza main.tf e adequa ao padrao da BD

--- main.tf
+++ main.tf
@@ -1,30 +1,31 @@
 module "bigquery-dataset-gasolina" {
   source  = "./modules/bigquery"
-  dataset_id                  = "gasolina_brasil"
-  dataset_name                = "gasolina_brasil"
-  description                 = "Dataset a respeito do histórico de preços da Gasolina no Brasil a partir de 2004"
+  
+  dataset_id                  = "br_anp_precos_combustiveis"
+  dataset_name                = "br_anp_precos_combustiveis"
+  description                 = "A ANP acompanha os preços praticados por revendedores de combustíveis automotivos e de gás liquefeito de petróleo envasilhado em botijões de 13 quilos (GLP P13), por meio de uma pesquisa semanal de preços realizada por empresa contratada." # Descrição da BD
   project_id                  = var.project_id
   location                    = var.region
   delete_contents_on_destroy  = true
-  deletion_protection = false
+  deletion_protection         = false
   access = [
     {
-      role = "OWNER"
+      role          = "OWNER"
       special_group = "projectOwners"
     },
     {
-      role = "READER"
+      role          = "READER"
       special_group = "projectReaders"
     },
     {
-      role = "WRITER"
+      role          = "WRITER"
       special_group = "projectWriters"
     }
   ]
   tables=[
     {
-        table_id           = "tb_historico_combustivel_brasil",
-        description        = "Tabela com as informacoes de preço do combustível ao longo dos anos"
+        table_id           = "microdados",
+        description        = "Série Histórica de Preços de Combustíveis - a saber, gasolina, gasolina aditivada, etanol, diesel s10, diesel, gnv e glp - com base na pesquisa de preços da Agência Nacional do Petróleo, Gás Natural e Biocombustíveis realizada a partir da primeira semana de 2004 até os dias de hoje."
         time_partitioning  = {
           type                     = "DAY",
           field                    = "data",
@@ -32,14 +33,14 @@ module "bigquery-dataset-gasolina" {
           expiration_ms            = null
         },
         range_partitioning = null,
-        expiration_time = null,
-        clustering      = ["produto","regiao_sigla", "estado_sigla"],
-        labels          = {
+        expiration_time    = null,
+        clustering         = ["produto", "regiao_sigla", "estado_sigla"],
+        labels             = {
           name    = "stack_data_pipeline"
           project  = "gasolina"
         },
         deletion_protection = true
-        schema = file("./bigquery/schema/gasolina_brasil/tb_historico_combustivel_brasil.json")
+        schema = file("./bigquery/schema/br-anp-precos-combustiveis/microdados.json")
     }
   ]
 }

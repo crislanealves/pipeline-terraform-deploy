bf6a402e716e311646a478c3917f5cf646167edf
Crislane Alves <crislanealves@protonmail.com>
2023-03-05 01:04:25 +0000

    fix: fix api files

--- api/main.py
+++ api/main.py
@@ -1,8 +1,10 @@
+import uvicorn
+import requests
+
 from fastapi      import FastAPI, HTTPException
 from pydantic     import BaseModel
 from google.cloud import storage
 
-import requests
 
 app = FastAPI()
 
@@ -11,12 +13,29 @@ class Params(BaseModel):
     bucket_name: str
     output_file_prefix: str
 
-def get_data(url):
+def put_file_to_gcs(output_file: str, bucket_name: str, content):
+    """ Uploads the data to the Cloud Storage bucket """
+
+    try:
+        storage_client = storage.Client()
+        bucket         = storage_client.bucket(bucket_name)
+        blob           = bucket.blob(output_file)
+
+        blob.upload_from_string(content)
+
+        return 'OK'
+
+    except Exception as ex:
+        print(ex)
+
+
+def get_data(remote_url):
     """  Function used to download data """
 
-    response = requests.get(url)
+    response = requests.get(remote_url)
 
     return response
+
 
 @app.post("/download_data")
 async def download_data(params: Params):
@@ -30,5 +49,9 @@ async def download_data(params: Params):
             content     = data.content)
 
         return {"Status": "OK", "Bucket_name": params.bucket_name, "url": params.url}
+
     except Exception as ex:
         raise HTTPException(status_code=ex.code, detail=f"{ex}")
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080)

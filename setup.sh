330ec7eb59faf081b95e968616fa669ef9fa9c37
Crislane Alves <s.alvescrislane@gmail.com>
2023-06-25 13:41:07 +0000

    fix: setup and add gitignore

--- setup.sh
+++ setup.sh
@@ -1,28 +1,37 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-echo "Verificando compatibilidade do sistema operacional ..."
+# green
+green() {
+  echo -e "\033[1;32m$@\033[0m"
+}
+
+# blue
+blue() {
+  echo -e "\033[1;34m$@\033[0m"
+}
+  
+#install Google SDK
+
+blue "$(tput sitm)Verificando se o sistema operacional atende aos requisitos necessários ...$(tput sgr0)"
 
 sudo apt-get install apt-transport-https ca-certificates gnupg
 
-echo "Iniciando a instalação"
+green "$(tput bold)Requisitos verificados e aceitos com sucesso!$(tput sgr0)"
 
-echo "Passo 1: Executando comando de compatibilidade"
+blue "$(tput sitm)Adiciondo o URI de distribuição da CLI gcloud como uma origem de pacote.$(tput sgr0)"
 
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 
-echo "Passo 2: Importando a chave pública do Google Cloud"
+blue "$(tput sitm)Importando a chave pública do Google Cloud...$(tput sgr0)"
 
-#curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 
-echo "Passo 3: Atualizando e instalando a CLI gcloud"
+blue "$(tput sitm)Atualizando e instalando a CLI gcloud...$(tput sgr0)"
 
 sudo apt-get update && sudo apt-get install google-cloud-cli
+ 
+green "$(tput bold)Google SDK instalado com sucesso! $(tput sgr0)"
 
-echo "Passo 4: Autenticação"
+blue "$(tput bold)Clique no link abaixo para que o Google Cloud SDK acesse sua conta do Google$(tput sgr0)"
 
-gcloud auth application-default login
-
-
-
-
+gcloud auth login
